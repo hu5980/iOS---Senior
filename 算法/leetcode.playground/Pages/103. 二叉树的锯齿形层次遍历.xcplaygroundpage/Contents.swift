4755bc767bb96f44cbbaad5cd1948e6eeea211ef
胡光耀 <xinghen01@163.com>
2018-09-15 19:38:35 +0800

    算法

---
+++ 算法/leetcode.playground/Pages/103. 二叉树的锯齿形层次遍历.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
+    
+    func push
+    
+    var tree = [[Int]]()
+    
+    var layer = 0
+    
+    var tempArr = [TreeNode]()
+    
+    if root != nil {
+        var temp = [root!.val]
+        tree.append(temp)
+        if root?.left != nil {
+            tempArr.append(root?.left)
+        }
+        
+        if root?.right != nil {
+            tempArr.append(root?.right)
+        }
+    }
+    
+    
+    
+    
+    for x
+    
+    
+    
+    
+    return tree
+}
+
+zigzagLevelOrder

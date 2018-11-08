26800a2c367047e90d8f767fe02600e49ec8f30c
Bruce <hugy@500wan.com>
2018-11-08 20:30:18 +0800

    RXSwift 学习

--- RxTestProject/Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "RxTestProject",
-    target:[],
-    dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", "4.0.0" ..< "5.0.0")
-    ]
-)
--- RxTestProject/RXSwiftStudy/RXSwiftStudy/ViewController.swift
+++ RxTestProject/RXSwiftStudy/RXSwiftStudy/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
+    
+    let neverSequence = Observable<String>.never()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let neverSequenceSubsciption = neverSequence.subscribe { (_) in
+            print("This will never be printed")
+        }
+        
+        neverSequenceSubsciption.disposed(by: disposeBag)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 

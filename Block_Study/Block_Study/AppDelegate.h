be00427f2ca852b2b06d972298805e852b29a9fa
胡光耀 <hugy@500wan.com>
2018-11-29 17:49:05 +0800

    上传注释 以及源码

---
+++ Block_Study/Block_Study/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Block_Study
+//
+//  Created by Bruce on 2018/11/23.
+//  Copyright © 2018年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
